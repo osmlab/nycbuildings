95977c29e39fcdf1e261ef9cb30b793f25f76d22
lxbarth <alex@mapbox.com>
2014-01-12 16:37:21 -0800

    Fix output and tallies.

--- findranges.py
+++ findranges.py
@@ -4,19 +4,19 @@ from merge import merge
 import re
 
 def findRanges(buildings):
-    onlyRanges = 0 ## Instances where there are only range house numbers in a building
-    rangesAndOther = 0
+    onlyRanges = 0 ## Buildings with only range house numbers
+    rangesAndOther = 0 ## Buildings with range numbers and other numbers
     for building in buildings['data']:
         rangeNum = 0 # House number like "10 - 12"
         total = len(building['properties']['addresses'])
         for address in building['properties']['addresses']:
             if address['properties'][u'HOUSE_NU_2'] is not None:
                 rangeNum = rangeNum + 1
-            if rangeNum != 0 and rangeNum == total:
-                onlyRanges = onlyRanges + 1
-            else:
-                rangesAndOther = rangesAndOther + 1
-    print "%s   %s  %s" % (onlyRanges, rangesAndOther, len(buildings('data')))
+        if rangeNum != 0 and rangeNum == total:
+            onlyRanges = onlyRanges + 1
+        elif rangeNum != 0:
+            rangesAndOther = rangesAndOther + 1
+    print "%s,%s,%s" % (onlyRanges, rangesAndOther, len(buildings['data']))
 
 # Run conversions. Expects an chunks/addresses-[district id].shp for each
 # chunks/buildings-[district id].shp. Optinally convert only one election district.
