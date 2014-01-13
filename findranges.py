b3f718580848afe0d3d57d6f417451f64ad9a43f
lxbarth <alex@mapbox.com>
2014-01-12 16:10:18 -0800

    Tally number of buildings w/ only range addresses, range addresses and other addresses and total.

--- findranges.py
+++ findranges.py
@@ -1,12 +1,22 @@
 from sys import argv
 from glob import glob
 from merge import merge
-from pprint import pprint
 import re
 
 def findRanges(buildings):
+    onlyRanges = 0 ## Instances where there are only range house numbers in a building
+    rangesAndOther = 0
     for building in buildings['data']:
-        pprint(building['properties'])
+        rangeNum = 0 # House number like "10 - 12"
+        total = len(building['properties']['addresses'])
+        for address in building['properties']['addresses']:
+            if address['properties'][u'HOUSE_NU_2'] is not None:
+                rangeNum = rangeNum + 1
+            if rangeNum != 0 and rangeNum == total:
+                onlyRanges = onlyRanges + 1
+            else:
+                rangesAndOther = rangesAndOther + 1
+    print "%s   %s  %s" % (onlyRanges, rangesAndOther, len(buildings('data')))
 
 # Run conversions. Expects an chunks/addresses-[district id].shp for each
 # chunks/buildings-[district id].shp. Optinally convert only one election district.
