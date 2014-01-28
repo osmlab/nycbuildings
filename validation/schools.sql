cedbedcfebbea3d19118fbd7166cf1343b46bfa3
ingalls <nicholas.ingalls@gmail.com>
2014-01-28 18:36:59 -0400

    Fixed Query Error in schools.sql

--- validation/schools.sql
+++ validation/schools.sql
@@ -3,9 +3,11 @@
 --Removing the amenity and name tag and putting them on a polygon around the school yard.
 
 CREATE OR REPLACE VIEW buildings AS SELECT way_id FROM way_tags WHERE k='building';
-CREATE OR REPLACE VIEW schools AS SELECT way_id FROM way_tags WHERE k='amenity' AND v='school';
 
 DROP TABLE IF EXISTS school_errors;
 CREATE TABLE school_errors(id bigint NOT NULL);
 
-INSERT INTO school_errors SELECT schools.way_id FROM buildings, schools WHERE schools.way_id = schools.way_id;
+INSERT INTO school_errors SELECT buildings.way_id FROM buildings, way_tags WHERE k='amenity' AND 
+v='school' AND buildings.way_id = way_tags.way_id;
+
+DROP VIEW buildings;
