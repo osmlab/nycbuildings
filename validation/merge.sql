c014afdfcf5320efd2e6ef1a67765668ee10203b
ingalls <nicholas.ingalls@gmail.com>
2014-01-28 20:26:47 -0400

    Redesigning Merge Code For Efficiency

--- validation/merge.sql
+++ validation/merge.sql
@@ -2,48 +2,23 @@
 --This script will find these buildings and generate a list of way geometries that need correction
 --This script is not optimized and will literally take DAYS on a large extract! Use on a small area!
 
---PROCEDURE--
-CREATE OR REPLACE VIEW node_geom AS SELECT geom FROM nodes, node_tags WHERE k='addr:housenumber' AND id = node_id; 
+--createAreas MUST be run first!
 
---PROCESSING - ANY BUILDING WITH ANY # of ADDR NODES
-DROP TABLE IF EXISTS building_contains;
-CREATE TABLE building_contains();
-SELECT AddGeometryColumn('building_contains', 'geom', 4326, 'POLYGON', 2);
 
 --FINAL RESULTS - ANY BUILDING WITH 1 ADDR NODE
 DROP TABLE IF EXISTS building_merge;
 CREATE TABLE building_merge();
 SELECT AddGeometryColumn('building_merge', 'geom', 4326, 'POLYGON', 2);
 
-DROP TABLE IF EXISTS way_geom;
-CREATE TABLE way_geom();
-SELECT AddGeometryColumn('way_geom', 'geom', 4326, 'POLYGON', 2);
+--PROCESSING - ANY BUILDING WITH ANY # of ADDR NODES
+DROP TABLE IF EXISTS building_contains;
+CREATE TABLE building_contains();
+SELECT AddGeometryColumn('building_contains', 'geom', 4326, 'POLYGON', 2);CEDURE--
+CREATE OR REPLACE VIEW node_geom AS SELECT geom FROM nodes, node_tags WHERE k='addr:housenumber' AND id = node_id; 
 
 --FUNCTIONS--
-DROP FUNCTION IF EXISTS wayToGeom();
-DROP FUNCTION IF EXISTS contains();
 
-CREATE FUNCTION wayToGeom() RETURNS void AS $$
-	DECLARE
-		way_row RECORD;
-		wayID bigint;
-	BEGIN
-		FOR way_row IN SELECT * FROM way_tags WHERE k='building' ORDER BY way_id LOOP
-			wayID := way_row.way_id;
-			
-			INSERT INTO way_geom (geom) SELECT ST_MakePolygon(ST_MakeLine(geom ORDER BY sequence_id)) FROM (SELECT geom, sequence_id FROM nodes, way_nodes WHERE way_id = wayID AND node_id = id) way;
-		END LOOP;
-	EXCEPTION
-		WHEN others THEN
-            --This usually occurs when a line is tagged as a building
-            --Or when osmosis has broken a building when the file is split.
-            --The program must be started over if this error occurs
-            RAISE NOTICE 'ERROR ON: % REMOVING WAY', wayID;
-            DELETE FROM way_tags WHERE way_id = wayID;
-            DELETE FROM ways WHERE id = wayID;
-            DELETE FROM way_nodes WHERE way_id = wayID;
-END;
-$$ LANGUAGE plpgsql;
+DROP FUNCTION IF EXISTS contains();
 
 CREATE FUNCTION contains() RETURNS void AS $$
 	DECLARE
@@ -67,5 +42,5 @@ CREATE FUNCTION contains() RETURNS void AS $$
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT wayToGeom();
+
 SELECT contains();
