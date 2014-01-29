e5e1dc0475cb0f3763e796fa702c2df6be2d4d68
Aaron Lidman <aaronlidman@gmail.com>
2014-01-28 20:29:07 -0500

    Simplify a little

--- fix-ordinals/fix_ordinal_suffixes.py
+++ fix-ordinals/fix_ordinal_suffixes.py
@@ -1,38 +1,36 @@
-import sys
-import string
+from sys import argv, exit
 from lxml import etree
 
 def ordinal(n):
+    n = int(n)
     if 10 <= n % 100 < 20:
         return str(n) + 'th'
     else:
-       return str(n) + {1 : 'st', 2 : 'nd', 3 : 'rd'}.get(n % 10, 'th')
+       return  str(n) + {1 : 'st', 2 : 'nd', 3 : 'rd'}.get(n % 10, 'th')
 
-filename = sys.argv[1] if (len(sys.argv) > 1) else sys.exit('Invalid file name')
+filename = argv[1] if (len(argv) > 1) else exit('Invalid file name')
 tree = etree.parse(filename)
 addr_streets = tree.findall(".//tag[@k='addr:street']")
 
 for addr_street in addr_streets:
-    old_name = addr_street.get('v')
-    new_name = addr_street.get('v')
-    num_bef = ''
-    num_aft = ''
-    for i, c in enumerate(new_name):
-        if c.isdigit():
-            start = i
-            while i < len(new_name) and new_name[i].isdigit():
-                i += 1
-            num_bef = new_name[start:i]#obtiene el numero dentro de un string
-            if new_name[start+i:i+1].isspace(): #verifica si el caracter es de una forma ejemplo: 117th
-                num_aft = ordinal(int(num_bef))
-                new_name = new_name.replace(num_bef, num_aft)
+  name = addr_street.get('v')
 
-            if addr_street.get('v') != new_name:
-                addr_street.set('v', new_name)
-                parent = addr_street.getparent()
-                parent.attrib['action'] = 'modify'
-            break
-    print '- %s > %s' % (old_name, new_name)
+  for index, char in enumerate(name):
+    if char.isdigit():
+        start = index
+        end = index
+        while end < len(name) and name[end].isdigit():
+            end += 1
+        number = name[start:end]
+
+        if name[start+end:end+1].isspace():
+            name = ordinal(number) + name[0:start] + name[end:]
+
+        if name != addr_street.get('v'):
+            print  '- %s > %s' % (addr_street.get('v'), name)
+            addr_street.set('v', name)
+            parent = addr_street.getparent()
+            parent.attrib['action'] = 'modify'
 
 xml = "<?xml version='1.0' encoding='UTF-8'?>\n" + etree.tostring(tree, encoding='utf8').replace('"',"'")
 new_file = open(filename[:-4] + '_new' + filename[-4:], 'w')
