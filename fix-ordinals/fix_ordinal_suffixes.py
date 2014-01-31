a69421c55ef8e9b541cd9ea6b78e3843bfa98844
Aaron Lidman <aaronlidman@gmail.com>
2014-01-31 18:03:10 -0500

    Print useful details

--- fix-ordinals/fix_ordinal_suffixes.py
+++ fix-ordinals/fix_ordinal_suffixes.py
@@ -12,6 +12,8 @@ filename = argv[1] if (len(argv) > 1) else exit('Invalid file name')
 tree = etree.parse(filename)
 addr_streets = tree.findall(".//tag[@k='addr:street']")
 
+counts = {}
+
 for addr_street in addr_streets:
   name = addr_street.get('v')
 
@@ -27,10 +29,21 @@ for addr_street in addr_streets:
             name = ordinal(number) + name[0:start] + name[end:]
 
         if name != addr_street.get('v'):
-            print  '- %s > %s' % (addr_street.get('v'), name)
-            addr_street.set('v', name)
-            parent = addr_street.getparent()
-            parent.attrib['action'] = 'modify'
+            key = addr_street.get('v') + ' -> ' + name
+            if key in counts:
+                counts[key] += 1
+            else:
+                counts[key] = 1
+
+        addr_street.set('v', name)
+        parent = addr_street.getparent()
+        parent.attrib['action'] = 'modify'
+
+out = 'modification | number of items\n'
+out += '--- | ---\n'
+for name in counts:
+    out += name + ' | ' + str(counts[name]) + '\n'
+print out
 
 xml = "<?xml version='1.0' encoding='UTF-8'?>\n" + etree.tostring(tree, encoding='utf8').replace('"',"'")
 new_file = open(filename[:-4] + '_new' + filename[-4:], 'w')
