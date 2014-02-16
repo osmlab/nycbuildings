8e2d548ada30c84e8524960f51d0965415c2563f
lxbarth <alex@mapbox.com>
2014-02-16 14:03:37 -0500

    Only track queens / non-queens addresses where they coexist _for the same street name_.

--- validation/historicaddresses.js
+++ validation/historicaddresses.js
@@ -5,8 +5,9 @@
 // ulimit -n 7000 # set ulimit high enough to read all files
 // node validation/historicaddresses.js merged/* > historicaddresses.csv
 var fs = require('fs');
+var _ = require('underscore');
 
-console.log('address,specialcode,lng,lat');
+console.log('address,perbuilding,specialcode,lng,lat');
 process.argv.splice(2).forEach(function(file) {
     fs.readFile(file, function(err, data) {
         if (err) {
@@ -25,26 +26,43 @@ process.argv.splice(2).forEach(function(file) {
         var coordinates = [];
         buildings.forEach(function(building) {
             var addresses = building.properties['addresses'];
-            if (addresses.length > 2) {
+            var housenumbers =
+                _.chain(addresses)
+                .pluck('properties')
+                .reduce(function(memo, p) {
+                    memo[p['STREET_NAM']] = memo[p['STREET_NAM']] || [];
+                    memo[p['STREET_NAM']].push(p['HOUSE_NUMB']);
+                    return memo;
+                }, {});
+            var mixed = false;
+            _(housenumbers).each(function(numbers, street) {
                 var queens = 0;
                 var notQueens = 0;
-                addresses.forEach(function(address) {
-                    if (address.properties['HOUSE_NUMB'].indexOf('-') == -1) {
+                _(numbers).each(function(number) {
+                    if (number.indexOf('-') == -1) {
                         notQueens++;
                     } else {
                         queens++;
                     }
                 });
                 if (queens > 0 && notQueens > 0) {
-                    addresses.forEach(function(a) {
-                        console.log(
-                            a.properties['STREET_NAM'] + ' ' + a.properties['HOUSE_NUMB'] + ',' +
-                            a.properties['SPECIAL_CO'] + ',' +
-                            a.geometry.coordinates[0] + ',' +
-                            a.geometry.coordinates[1]
-                        );
-                    });
+                    mixed = true;
                 }
+            });
+            if (mixed) {
+                addresses.forEach(function(a) {
+                    var housenumber = a.properties['HOUSE_NUMB'];
+                    if (a.properties['HOUSE_NU_1']) {
+                        housenumber += ' ' + a.properties['HOUSE_NU_1'];
+                    }
+                    console.log(
+                        housenumber + ' ' + a.properties['STREET_NAM'] + ',' +
+                        addresses.length + ',' +
+                        a.properties['SPECIAL_CO'] + ',' +
+                        a.geometry.coordinates[0] + ',' +
+                        a.geometry.coordinates[1]
+                    );
+                });
             }
         });
     });
