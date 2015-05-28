75e4871f3fe8e9528f4f26b470dfa98a3259f5a1
Rub21 <ruben@mapbox.com>
2015-05-28 21:20:39 +0530

    change url

--- fixes/clean-up-special_co-address/index.js
+++ fixes/clean-up-special_co-address/index.js
@@ -10,16 +10,17 @@ _.each(grid.features, function(val) {
 });
 
 function url_download_blocks(val) {
-		var bbox = +val.properties.left + "," + val.properties.top + "," + val.properties.right + "," + val.properties.bottom;
-		var url = "http://127.0.0.1:8111/import?url=http://api.openstreetmap.org/api/0.6/map?bbox=";
-		url = url + bbox;
-		var text = '- [ ] [ Block - ' + val.properties.id + '](' + url + ') \n';
-		if (val.properties.id <= 100) fs.appendFile(argv.geofile.split('.')[0] + "-urls-1.md", text, function(err) {});
-		if (val.properties.id > 100 && val.properties.id <= 200) fs.appendFile(argv.geofile.split('.')[0] + "-urls-2.md", text, function(err) {});
-		if (val.properties.id > 200 && val.properties.id <= 300) fs.appendFile(argv.geofile.split('.')[0] + "-urls-3.md", text, function(err) {});
-		if (val.properties.id > 300 && val.properties.id <= 400) fs.appendFile(argv.geofile.split('.')[0] + "-urls-4.md", text, function(err) {});
-		if (val.properties.id > 400) fs.appendFile(argv.geofile.split('.')[0] + "-urls-5.md", text, function(err) {});
-	}
+	//var bbox = +val.properties.left + "," + val.properties.top + "," + val.properties.right + "," + val.properties.bottom;
+	//var url = "http://127.0.0.1:8111/import?url=http://api.openstreetmap.org/api/0.6/map?bbox=";
+	var url = 'http://127.0.0.1:8111/load_and_zoom?left=' + val.properties.left + '&bottom=' + val.properties.top + '&right=' + val.properties.right + '&top=' + val.properties.bottom;
+	//url = url + bbox;
+	var text = '- [ ] [ Block - ' + val.properties.id + '](' + url + ') \n';
+	if (val.properties.id <= 100) fs.appendFile(argv.geofile.split('.')[0] + "-urls-1.md", text, function(err) {});
+	if (val.properties.id > 100 && val.properties.id <= 200) fs.appendFile(argv.geofile.split('.')[0] + "-urls-2.md", text, function(err) {});
+	if (val.properties.id > 200 && val.properties.id <= 300) fs.appendFile(argv.geofile.split('.')[0] + "-urls-3.md", text, function(err) {});
+	if (val.properties.id > 300 && val.properties.id <= 400) fs.appendFile(argv.geofile.split('.')[0] + "-urls-4.md", text, function(err) {});
+	if (val.properties.id > 400) fs.appendFile(argv.geofile.split('.')[0] + "-urls-5.md", text, function(err) {});
+}
 
 //bellow code is used to search for addresses using Overpass Api run 
 // node index.js --csvfile=a.csv --geofile=a.geojson
